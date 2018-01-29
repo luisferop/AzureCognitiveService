353a2ce38e69eb09dad5fc28c921267139331d4a
Luís Pereira <luisferop@gmail.com>
2018-01-29 21:01:08 -0200

    Creating project to learn how cognitve service works.

---
+++ prjPresentation/frmMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace prjPresentation
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBoxVideo = new System.Windows.Forms.PictureBox();
+            this.btnTakePhoto = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxVideo
+            // 
+            this.picBoxVideo.Location = new System.Drawing.Point(12, 29);
+            this.picBoxVideo.Name = "picBoxVideo";
+            this.picBoxVideo.Size = new System.Drawing.Size(1097, 564);
+            this.picBoxVideo.TabIndex = 0;
+            this.picBoxVideo.TabStop = false;
+            // 
+            // btnTakePhoto
+            // 
+            this.btnTakePhoto.Location = new System.Drawing.Point(307, 623);
+            this.btnTakePhoto.Name = "btnTakePhoto";
+            this.btnTakePhoto.Size = new System.Drawing.Size(224, 23);
+            this.btnTakePhoto.TabIndex = 1;
+            this.btnTakePhoto.Text = "Smile :)";
+            this.btnTakePhoto.UseVisualStyleBackColor = true;
+            this.btnTakePhoto.Click += new System.EventHandler(this.btnTakePhoto_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(575, 623);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(236, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1121, 686);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnTakePhoto);
+            this.Controls.Add(this.picBoxVideo);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Captue Video";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxVideo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxVideo;
+        private System.Windows.Forms.Button btnTakePhoto;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ prjPresentation/frmMain.cs
@@ -0,0 +1,59 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjPresentation
+{
+    public partial class frmMain : Form
+    {
+        VideoCaptureDevice videoSource;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            AForge.Video.DirectShow.FilterInfoCollection videosources = new AForge.Video.DirectShow.FilterInfoCollection(AForge.Video.DirectShow.FilterCategory.VideoInputDevice);
+
+            if (videosources != null)
+            {
+                videoSource = new AForge.Video.DirectShow.VideoCaptureDevice(videosources[0].MonikerString);
+                videoSource.NewFrame += (s, e) =>
+                {
+                    if (picBoxVideo.Image != null)
+                    {
+                        picBoxVideo.Image.Dispose();
+                    }
+
+                    picBoxVideo.Image = (Bitmap)e.Frame.Clone();
+                };
+                videoSource.Start();
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnTakePhoto_Click(object sender, EventArgs e)
+        {
+            picBoxVideo.Image.Save("snapshot.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource = null;
+            }
+        }
+    }
+}
